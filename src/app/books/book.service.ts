56332ec8260c5bfd18f9f7516b8c6fe546460785
gwanjari <gwanjari@apple.com>
2019-10-14 12:02:27 -0700

    Added scrolling to book list and more books

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -29,7 +29,34 @@ export class BookService {
         "https://en.wikipedia.org/wiki/Divine_Comedy\n",
         928,
         "The Divine Comedy",
-        1315)];
+        1315),
+        new Book("Unknown",
+        "Sumer and Akkadian Empire",
+        "../../../assets/the-epic-of-gilgamesh.jpg",
+        "Akkadian",
+        "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+        160,
+        "The Epic Of Gilgamesh",
+        1700),
+        new Book(
+        "Unknown",
+        "Achaemenid Empire",
+        "../../../assets/the-book-of-job.jpg",
+        "Hebrew",
+        "https://en.wikipedia.org/wiki/Book_of_Job\n",
+        176,
+        "The Book Of Job",
+        600),
+        new Book(
+        "Unknown",
+        "India/Iran/Iraq/Egypt/Tajikistan",
+        "../../../assets/one-thousand-and-one-nights.jpg",
+        "Arabic",
+        "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights\n",
+        288,
+        "One Thousand and One Nights",
+        1200)];
+    
 
     getBooks(){
         return this.books;
